dc1b2b00b16ce2ad436383a764e41e38fd556da0
AFADJARR <achmad.fadjar.ft24@mail.umy.ac.d>
2025-03-26 12:34:55 +0700

    membuat looping dengan j dimulai dari 0

--- insertionshort.cpp
+++ insertionshort.cpp
@@ -65,5 +65,9 @@ void display()
     cout << "Element array yang telah tersusun" << endl; // Output ke layar
     cout << "=======================" << endl; // Output ke layar
     
-    for
+    for(int j = 0; j < n; j++)
+    { // Looping dengan j dimulai dari 0 hingga n-1
+        cout << arr[j] <<endl; // Output ke layar
+    }
+    cout << endl; // Output baris kosong
 }
