3d2729b59a8550225a3be48fa1d5954081a0c5e2
AFADJARR <achmad.fadjar.ft24@mail.umy.ac.d>
2025-03-26 12:14:14 +0700

    membuat inputan jumlah array dan memamnggil variable inputan n

--- insertionshort.cpp
+++ insertionshort.cpp
@@ -3,3 +3,25 @@ using namespace std;
 
 int arr[20]; // Membuuat Array dengan panjang data 20
 int n; // Membuat variable inputan n
+
+void input ()
+{ // Procedure Input
+    wihle(true)
+    {
+        cout << "Masukan jumlah data pada array : "; // Membuat inputan jumlahh element Array 
+        cin >> n; // Memanggil vatriable inputan n
+
+        if (n <= 20)
+        {
+            // Membuat kondisi n tidak lebih dari 20
+            break;
+        }
+        else
+        {
+            cout << "\nArray yang anda masukan maksimal 20 elemen.\n"; // Menampilkan pesan jika data lebih dari 20
+        }
+    }
+    cout << endl; // Membuat jarak per baris program
+    cout << "======================="
+
+}
